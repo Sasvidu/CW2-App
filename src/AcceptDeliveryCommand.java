d8efac33204124d2f8d7b6f56a1fad355c96aa88
Sasvidu Ranthul <66816946+Sasvidu@users.noreply.github.com>
2024-01-17 01:10:13 +0530

    Add Dummy MappingAPI

--- src/AcceptDeliveryCommand.java
+++ src/AcceptDeliveryCommand.java
@@ -1,9 +1,11 @@
+import java.util.Locale;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class AcceptDeliveryCommand implements Command {
 
     private String deliverer;
+    private String destination;
 
     public AcceptDeliveryCommand(String deliverer) {
         this.deliverer = deliverer;
@@ -18,11 +20,16 @@ public class AcceptDeliveryCommand implements Command {
         String orderId = scanner.nextLine().trim();
         Order order = orderRepository.getOrder(orderId);
 
+        System.out.println("Enter the destination for the delivery: ");
+        this.destination = scanner.nextLine().trim();
+
         if (order != null && order.getState() instanceof OrderOutForDeliveryState) {
             order.process();
             OrderInDeliveryState state = (OrderInDeliveryState) order.getState();
             state.setDeliverer(deliverer);
             DeliveryApp.getInstance().AddDeliveryMapEntry(order);
+            String estimatedTime = MappingAPI.getInstance().getEstimatedTravelTime(Main.dummyOrigin, this.destination);
+            System.out.println("Estimated Time: " + estimatedTime);
             System.out.println("");
         } else {
             throw new NoSuchElementException("Order not found or not ready for delivery.");
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 
+    public static final String dummyOrigin = "origin";
+
     public static void main(String[] args) {
         System.out.println("\n\nApp Started!\n");
         initializeLoginData();
